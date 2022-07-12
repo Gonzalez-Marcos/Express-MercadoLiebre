af4936ab83dec92afc1cffed06c1bb71ca182ed4
BARBARA RIOS <gonzalezmarcos_90@hotmail.com>
2022-07-12 20:51:43 -0300

    Subida de ML

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+
+
+app.use(express.static(publicPath));
+app.use(express.urlencoded());
+
+// Este resolve nos permite enviar cualquier HTML a nuestro sitio web.
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
+app.listen(3030, () => {
+    console.log("Servidor iniciado en http://localhost:3030")
+});
